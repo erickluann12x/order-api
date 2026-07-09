4aa63aaf6e765259255700f62d9738a21db1c56c
ErickLuan <erickluan12x@gmail.com>
2026-07-08 23:08:11 -0300

    Create request wholesale

---
+++ order-api/src/main/java/com/erick/order_api/dto/WholesaleRequestDTO.java
@@ -0,0 +1,13 @@
+package com.erick.order_api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
+public record WholesaleRequestDTO(@NotBlank(message = "Nome do Cliente é obrigatorio") String nomeCliente,
+                                  @NotBlank(message = "Nome do Vendedor é obrigatorio") String nomeVendedor,
+                                  @NotBlank(message = "Nome da Marca é obrigatorio") String marca,
+BigDecimal valorTotal, Integer numeroCliente, MultipartFile foto) {
+
+}
